96f08940516c23001de417c8e7cc54c7383749bf
Sagar Bharadwaj <skalasib@andrew.cmu.edu>
2024-07-18 02:16:48 -0400

    Added search bar and polygon drawing tool for map

--- map-js/src/init_map.js
+++ map-js/src/init_map.js
@@ -1,11 +1,30 @@
-import L from 'leaflet';
-
+// leaflet, leaflet-geoman, and leaflet-geosearch are imported in index.html as including them in 
+// the bundle makes the bundle size too large
 function initialize_leaflet_map() {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([40.44266, -79.94327], 17);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
+    // Add toolbar
+    map.pm.addControls({
+        drawMarker: false,
+        drawCircleMarker: false,
+        drawPolyline: false,
+        drawRectangle: false,
+        drawPolygon: true,
+        drawCircle: false,
+        drawText: false,
+    });
+
+    //Add search bar
+    const provider = new GeoSearch.OpenStreetMapProvider();
+    const searchControl = new GeoSearch.GeoSearchControl({
+        provider: provider,
+        style: 'bar',
+    });
+    map.addControl(searchControl);
 }
 
 export { initialize_leaflet_map };
