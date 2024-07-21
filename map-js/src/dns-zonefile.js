11eb50c22afb97546edd77433e14740550865535
Sagar Bharadwaj <skalasib@andrew.cmu.edu>
2024-07-21 01:22:09 -0400

    Download DNS zone file

--- map-js/src/dns-zonefile.js
+++ map-js/src/dns-zonefile.js
@@ -83,7 +83,15 @@ function generateZoneFileFromDOM() {
     if (!zoneFileText) {
         return null;
     }
-    console.log(zoneFileText);
+    // Download the zone file
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(zoneFileText));
+    element.setAttribute('download', 'dns-zonefile.txt');
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+
     return zoneFileText;
 }
 
