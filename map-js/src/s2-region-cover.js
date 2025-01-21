2595924a5eed6534182861a8502ce16548d79886
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2025-01-20 23:53:08 -0500

    S2 region coverer works for both anti-clockwise and clockwise point selections

--- map-js/src/s2-region-cover.js
+++ map-js/src/s2-region-cover.js
@@ -7,8 +7,23 @@ function getS2RegionCover(maxCells, polygonCoords) {
     var maxLevel = 30;
     var maxCells = maxCells;
 
+    // Check if the points are in clockwise order
+    if (checkAntiClockwise(points)) {
+        points = points.reverse();
+    }
     var cells = GetS2CellsInRegion(points, minLevel, maxLevel, maxCells);
     return cells;
+}
+
+function checkAntiClockwise(points) {
+    let p1 = points[0];
+    let p2 = points[1];
+    let p3 = points[2];
+
+    let v1 = { x: p2.Lng - p1.Lng, y: p2.Lat - p1.Lat };
+    let v2 = { x: p3.Lng - p2.Lng, y: p3.Lat - p2.Lat };
+
+    return (v1.x * v2.y - v1.y * v2.x) < 0;
 }
 
 // Draw the S2 region cover on the map
